eeae223d74f0359e86c10266b7403e24defd08f5
pufington <ganesh289katta@gmail.com>
2023-08-01 16:10:46 +0530

    .

--- restapi/src/main/java/com/example/restapi/RestapiApplication.java
+++ restapi/src/main/java/com/example/restapi/RestapiApplication.java
@@ -2,8 +2,13 @@ package com.example.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableWebMvc
+@EnableSwagger2
 @SpringBootApplication
+
 public class RestapiApplication {
 
 	public static void main(String[] args) {
